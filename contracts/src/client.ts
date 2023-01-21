88ce5947555e90ad9df0dccc0489250007869c5a
Amovane <amovane@163.com>
2023-01-21 23:06:52 +0800

    Chore: bump @imart/contracts version to 0.0.3

--- contracts/src/client.ts
+++ contracts/src/client.ts
@@ -1,9 +1,13 @@
 import { Config } from "./types";
 import { ContractProxy } from "./proxy";
+import { Aptos } from "./aptos";
+import { Evm } from "./evm";
 
-export function Contractor<T extends ContractProxy>(
-  Impl: new (config: Config) => T,
+export type Contract = Aptos | Evm;
+
+export function Contractor<Contract extends ContractProxy>(
+  Impl: new (config: Config) => Contract,
   config: Config
-): T {
+): Contract {
   return new Impl(config);
 }
--- contracts/src/index.ts
+++ contracts/src/index.ts
@@ -2,3 +2,5 @@ export * from "./typechain";
 export * from "./client";
 export * from "./proxy";
 export * from "./types";
+export * from "./evm";
+export * from "./aptos";
