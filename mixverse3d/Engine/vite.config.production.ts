d590aa9ae0388b719a34847a0af3bb311b3498eb
hanyuhui <1608407759@qq.com>
2023-03-06 19:16:07 +0800

    upload

--- mixverse3d/Engine/vite.config.production.ts
+++ mixverse3d/Engine/vite.config.production.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       formats: ["es"]
     },
     rollupOptions: {
-      external: ["react", "react-dom",'@ungap/structured-clone','stats.js','wavesurfer.js']
+      external: ["react", "react-dom"]
     }
   },
   publicDir: false
