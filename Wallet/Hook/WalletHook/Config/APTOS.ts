ef0cee4fbf966163602b98f5e552f0d453b66fab
Amovane <amovane@163.com>
2023-03-09 15:15:28 +0800

    chore: tweak contracts

--- Wallet/Hook/WalletHook/Config/APTOS.ts
+++ Wallet/Hook/WalletHook/Config/APTOS.ts
@@ -1,11 +1,11 @@
 export const APTOS_MARKET_ADDRESS =
-    "0x5e895990e6134dc0e5774090b5657410e78713a1ae4839870857a1b57bd370b4";
+    "0x1c90da7e4cccf4d612f27dcbc72d142218f75ab8bc877a7331dcade480673f2c";
 export const APTOS_SINGLE_COLLECTIVE_ADDRESS =
-    "0x0d42d82754ff68a86b11e10ffb91e8bda89a8b14f587dce7244b97279d19a965";
+    "0x1c90da7e4cccf4d612f27dcbc72d142218f75ab8bc877a7331dcade480673f2c";
 export const APTOS_MULTIPLE_COLLECTIVE_ADDRESS =
-    "0x0d42d82754ff68a86b11e10ffb91e8bda89a8b14f587dce7244b97279d19a965";
+    "0x1c90da7e4cccf4d612f27dcbc72d142218f75ab8bc877a7331dcade480673f2c";
 export const APTOS_CURATION_ADDRESS =
-    "0x5e895990e6134dc0e5774090b5657410e78713a1ae4839870857a1b57bd370b4";
+    "0x1c90da7e4cccf4d612f27dcbc72d142218f75ab8bc877a7331dcade480673f2c";
 
 export const NODE_URL = "https://fullnode.testnet.aptoslabs.com";
 export const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
