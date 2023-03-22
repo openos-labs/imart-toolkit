1d471cc8bea47d677b3d59f70b7d5a083c655923
Amovane <amovane@163.com>
2023-03-22 12:04:16 +0800

    chore: tweak curation contract address

--- Wallet/Hook/WalletHook/Config/APTOS.ts
+++ Wallet/Hook/WalletHook/Config/APTOS.ts
@@ -5,7 +5,7 @@ export const APTOS_SINGLE_COLLECTIVE_ADDRESS =
 export const APTOS_MULTIPLE_COLLECTIVE_ADDRESS =
     "0x1c90da7e4cccf4d612f27dcbc72d142218f75ab8bc877a7331dcade480673f2c";
 export const APTOS_CURATION_ADDRESS =
-    "0x1c90da7e4cccf4d612f27dcbc72d142218f75ab8bc877a7331dcade480673f2c";
+    "0xe544fe1018243182cafe3fab04a07f82edf83b98a0c9a217db220660fe689f37";
 
 export const NODE_URL = "https://fullnode.testnet.aptoslabs.com";
 export const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
