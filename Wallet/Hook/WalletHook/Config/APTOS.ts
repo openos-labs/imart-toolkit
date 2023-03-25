38936bf4d3027de5a34aff8693a76b38d69c8f69
Amovane <amovane@163.com>
2023-03-26 04:36:02 +0800

    chore: bump toolkit version to 0.3.3

--- Wallet/Hook/WalletHook/Config/APTOS.ts
+++ Wallet/Hook/WalletHook/Config/APTOS.ts
@@ -1,11 +1,11 @@
 export const APTOS_MARKET_ADDRESS =
-    "73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
+    "0x73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
 export const APTOS_SINGLE_COLLECTIVE_ADDRESS =
-    "73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
+    "0x73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
 export const APTOS_MULTIPLE_COLLECTIVE_ADDRESS =
-    "73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
+    "0x73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
 export const APTOS_CURATION_ADDRESS =
-    "73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
+    "0x73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
 
 export const NODE_URL = "https://fullnode.testnet.aptoslabs.com";
 export const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
