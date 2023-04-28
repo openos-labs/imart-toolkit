3a430188102e86e20c21bac1bef07e67911d760c
Amovane <amovane@163.com>
2023-04-28 19:11:52 +0800

    chore: bump toolkit/wallet version to 0.3.20

--- Wallet/Hook/WalletHook/Config/APTOS.ts
+++ Wallet/Hook/WalletHook/Config/APTOS.ts
@@ -1,11 +1,11 @@
 export namespace AptosSpec {
     export const NODE_URL = "https://fullnode.testnet.aptoslabs.com";
     export const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
-    export const MARKET = "0x73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
-    export const SINGLE_COLLECTIVE = "0x73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
-    export const MULTIPLE_COLLECTIVE ="0x73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
-    export const MIXVERSE_SPACE = "0x73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
-    export const CURATION = "0x73545daede11168cbe4f5503f19929c7eebb888cffef27983ed374515f7e82a1";
+    export const MARKET = "0x0ae196d71a77b77e151a8fae5af27ced2cd16b7f6ed951065b51faed40544bf8";
+    export const SINGLE_COLLECTIVE = "0x0ae196d71a77b77e151a8fae5af27ced2cd16b7f6ed951065b51faed40544bf8";
+    export const MULTIPLE_COLLECTIVE ="0x0ae196d71a77b77e151a8fae5af27ced2cd16b7f6ed951065b51faed40544bf8";
+    export const MIXVERSE_SPACE = "0x0ae196d71a77b77e151a8fae5af27ced2cd16b7f6ed951065b51faed40544bf8";
+    export const CURATION = "0x0ae196d71a77b77e151a8fae5af27ced2cd16b7f6ed951065b51faed40544bf8";
     export const configs ={
         testnet: {
             addresses: {
