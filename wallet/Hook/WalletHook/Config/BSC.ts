2165cabd7ae3ae68b43218ca61088fa73cba1d83
zy <yunstv@163.com>
2023-08-23 15:15:37 +0800

    feat: addresses add field quickDraw

--- wallet/Hook/WalletHook/Config/BSC.ts
+++ wallet/Hook/WalletHook/Config/BSC.ts
@@ -1,18 +1,21 @@
 export namespace BscSpec {
-    export const MARKET = "0x00000000000001ad428e4906aE43D8F9852d0dD6";
-    export const SINGLE_COLLECTIVE = "0x3A43fAb6DA76152a4F3A73CA4D36ACa3e5Bb098a";
-    export const MULTIPLE_COLLECTIVE ="0x787D3d66552C3F90574391ffd12F791725E23765";
-    export const CURATION = "0x846DcAd71124046E146bd01DcA744835EF216eb2";
-    export const NFTLOTTERY = "";
-    export const configs = {
-      testnet: {
-        addresses: {
-          singleCollective: SINGLE_COLLECTIVE,
-          multipleCollective: MULTIPLE_COLLECTIVE,
-          curation: CURATION,
-          market: MARKET,
-          nftlottery: NFTLOTTERY
-        },
-    }
-  }
+  export const MARKET = "0x00000000000001ad428e4906aE43D8F9852d0dD6";
+  export const SINGLE_COLLECTIVE = "0x3A43fAb6DA76152a4F3A73CA4D36ACa3e5Bb098a";
+  export const MULTIPLE_COLLECTIVE =
+    "0x787D3d66552C3F90574391ffd12F791725E23765";
+  export const CURATION = "0x846DcAd71124046E146bd01DcA744835EF216eb2";
+  export const QUICKDRAW = "0xE092DF7D1F7B43edd29C384a84c62E731F04A06b";
+  export const NFTLOTTERY = "";
+  export const configs = {
+    testnet: {
+      addresses: {
+        singleCollective: SINGLE_COLLECTIVE,
+        multipleCollective: MULTIPLE_COLLECTIVE,
+        curation: CURATION,
+        market: MARKET,
+        nftlottery: NFTLOTTERY,
+        quickDraw: QUICKDRAW,
+      },
+    },
+  };
 }
--- wallet/Hook/WalletHook/Config/POLYGON.ts
+++ wallet/Hook/WalletHook/Config/POLYGON.ts
@@ -1,7 +1,8 @@
 export namespace PolygonSpec {
   export const MARKET = "";
   export const SINGLE_COLLECTIVE = "0xbCBA93211d12F8B8416aCA0F5b1184c918a58DFe";
-  export const MULTIPLE_COLLECTIVE ="0x0f3ED7652caB58F0c9D79f97fF3eB13b307c5fdC";
+  export const MULTIPLE_COLLECTIVE =
+    "0x0f3ED7652caB58F0c9D79f97fF3eB13b307c5fdC";
   export const CURATION = "0x9fdCcb58448A75ae5e04527B0afF5939f47448EA";
   export const NFTLOTTERY = "";
   export const configs = {
@@ -11,8 +12,9 @@ export namespace PolygonSpec {
         multipleCollective: MULTIPLE_COLLECTIVE,
         curation: CURATION,
         market: MARKET,
-        nftlottery: NFTLOTTERY
+        nftlottery: NFTLOTTERY,
+        quickDraw: "",
       },
-    }
-  }
+    },
+  };
 }
