3c55fab62de80ec02f4294a632fd28b013fa4219
zy <yunstv@163.com>
2023-08-28 22:12:22 +0800

    feat: wallet chain uniform access

--- wallet/Hook/WalletHook/Config/BSC.ts
+++ wallet/Hook/WalletHook/Config/BSC.ts
@@ -1,20 +1,35 @@
+import { BSCAddresses } from "./addresses.config";
+
+const { testnet, mainnet } = BSCAddresses;
+const network = import.meta.env.NETWORK;
+const thatNetAddress = BSCAddresses[network];
+
 export namespace BscSpec {
-  export const MARKET = "0x00000000000001ad428e4906aE43D8F9852d0dD6";
-  export const SINGLE_COLLECTIVE = "0x7CEd67BfB6b25690C3d46a0773679cbeB0e059A6";
-  export const MULTIPLE_COLLECTIVE =
-    "0x281AA0645393Da4B81F3281F4a4932d232c57aaB";
-  export const CURATION = "0x9677bE3ca5ee3F5973D91FF053F0BA1eCa59809F";
-  export const QUICKDRAW = "0x91db77bBe3a79b654137f58157C41267A9830792";
-  export const NFTLOTTERY = "";
+  export const MARKET = thatNetAddress.MARKET;
+  export const SINGLE_COLLECTIVE = thatNetAddress.SINGLE_COLLECTIVE;
+  export const MULTIPLE_COLLECTIVE = thatNetAddress.MULTIPLE_COLLECTIVE;
+  export const CURATION = thatNetAddress.CURATION;
+  export const QUICKDRAW = thatNetAddress.QUICKDRAW;
+  export const NFTLOTTERY = thatNetAddress.NFTLOTTERY;
   export const configs = {
     testnet: {
       addresses: {
-        singleCollective: SINGLE_COLLECTIVE,
-        multipleCollective: MULTIPLE_COLLECTIVE,
-        curation: CURATION,
-        market: MARKET,
-        nftlottery: NFTLOTTERY,
-        quickDraw: QUICKDRAW,
+        singleCollective: testnet.SINGLE_COLLECTIVE,
+        multipleCollective: testnet.MULTIPLE_COLLECTIVE,
+        curation: testnet.CURATION,
+        market: testnet.MARKET,
+        nftlottery: testnet.NFTLOTTERY,
+        quickDraw: testnet.QUICKDRAW,
+      },
+    },
+    mainnet: {
+      addresses: {
+        singleCollective: mainnet.SINGLE_COLLECTIVE,
+        multipleCollective: mainnet.MULTIPLE_COLLECTIVE,
+        curation: mainnet.CURATION,
+        market: mainnet.MARKET,
+        nftlottery: mainnet.NFTLOTTERY,
+        quickDraw: mainnet.QUICKDRAW,
       },
     },
   };
